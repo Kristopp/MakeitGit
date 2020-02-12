7808cf5368eb6cb38c75365972fd8bd1fbab59b4
kristo palu <kristo.palu89@gmail.com>
2020-02-12 19:21:50 +0200

    roll

--- src/Layout/newCard.js
+++ src/Layout/newCard.js
@@ -1,7 +1,16 @@
 import { html } from './base'
+import state from '../index.js'
+
+
+export const getInputName = () => html.addName.value;
+export const getStepCount = () => html.addSteps.value;
+//here to red error thing 
+console.log(getInputName())
+//steps value 
+
 export const newCardMarkUp = () => { 
     const markup = `<div class="floating-main-card" id=>
-    <span class="card-text-name">Mission name</span>
+    <span class="card-text-name">${getInputName()}</span>
     <div class="steps-container" id="card-steps">
       <ul class="progressbar" id="progressbar">
         <li class="active" id="active">
@@ -12,3 +21,4 @@ export const newCardMarkUp = () => {
 
     html.doListContainer.insertAdjacentHTML('beforeend', markup)
 } 
+console.log(state)
--- src/index.js
+++ src/index.js
@@ -5,32 +5,27 @@ import * as inputValues from './Layout/inputValues';
 import * as newCard from './Layout/newCard'
 
 //Global state
-const state = [];
+export const state = [];
 
 const addNewMission = () => { 
-    console.log(state)
-    
     //Get values 
-    const nameValue = inputValues.getInputName();
     const stepValue = inputValues.getStepcount();
-    if(nameValue,stepValue >= 1 || stepValue < 5) { 
-        let mission = new Mission(nameValue, stepValue);
+    if(inputValues.getInputName(),stepValue >= 1 || stepValue < 5) { 
+        //Create new card and close modal
+        let mission = new Mission(inputValues.getInputName(), stepValue);
         state.push(mission);
-        newCard.newCardMarkUp();
         html.modal.style.display = "none";
+        //inputValues.clearInput();
     } else { 
         console.log('error')
     }
-    //Create new card
     //extract stepscount 
     
     //dynamic progressBar
-    //loop through object to get values
-    let name = state.map(e => e.newMissionName);
-    let steps = state.map(e =>e.stepsCount)
-    console.log(name,steps)
-
-//Create new mission on click
+    //loop through object to get input values
+    newCard.newCardMarkUp(); 
+    
+    console.log(state)
 }
 /*open close modal*/
 function openModal() {
